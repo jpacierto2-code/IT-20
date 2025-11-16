6764bbfae0c3a341c1608fa9136ab543c8e1ee72
chrischanluceno-jpg <chrischanluceno@gmail.com>
2025-11-17 07:51:29 +0800

    first commit

---
+++ math.js
@@ -0,0 +1,16 @@
+num1 = 2;
+num2 = 3;
+
+sum - num1 + num2;
+
+console.log(sum);
+
+function sum(num1,num2){
+        return num1 + num2
+}
+
+//input
+result = sum(3,3);
+
+//out put
+console.log(result);
