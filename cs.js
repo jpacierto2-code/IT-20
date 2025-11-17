adbd15c7f4942ae7e7b5a702bc7159d81a8000fb
chrischanluceno-jpg <chrischanluceno@gmail.com>
2025-11-17 08:23:45 +0800

    fourth commit

--- cs.js
+++ cs.js
@@ -8,3 +8,21 @@ if(age >= 18){
 
 }
 legalAge(50);
+
+function computeGrade(grade){
+
+if(grade >= 90){
+    console.log("1");
+}else if (grade >=90){
+    console.log("1.26");
+    }else if (grade >=85){
+         console.log("1.5");
+          }else if (grade >=80){
+             console.log("1.75");
+          } else {
+            console.log("Not within the transmutation")
+          }      
+}
+computeGrade(85);
+          
+    
